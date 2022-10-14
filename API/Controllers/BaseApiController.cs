61fbbaa78818be0d94e8749ef631ac726a71121e
jhernandez5 <jonathan.hernandez2@mmmhc.com>
2022-10-14 08:46:27 -0400

    Adding Edit,Delete,Update from front end

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MediatR;
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController:ControllerBase
     {
-        
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
